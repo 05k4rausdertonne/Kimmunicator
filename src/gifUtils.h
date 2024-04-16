ec88f7a48a763d9ee004c3201b4bca0dc634391f
05k4rausdertonne <oskarflath@gmail.com>
2024-04-16 17:01:13 +0200

    first commit

---
+++ src/gifUtils.h
@@ -0,0 +1,120 @@
+#include "Arduino.h"
+#include <bb_spi_lcd.h>
+#include <AnimatedGIF.h>
+#include <SD.h>
+#include "FS.h"
+
+#define LCD DISPLAY_CYD
+
+AnimatedGIF gif;
+BB_SPI_LCD lcd;
+int iOffX, iOffY;
+
+// file for AnimatedGIF lib
+File f;
+
+uint8_t *pFrameBuffer;
+
+// Draw callback from GIF decoder
+//
+// called once for each line of the current frame
+// MCUs with very little RAM would have to test for disposal methods, transparent pixels
+// and translate the 8-bit pixels through the palette to generate the final output.
+// The code for MCUs with enough RAM is much simpler because the AnimatedGIF library can
+// generate "cooked" pixels that are ready to send to the display
+
+void GIFDraw(GIFDRAW *pDraw)
+{
+  if (pDraw->y == 0) { // set the memory window when the first line is rendered
+    lcd.setAddrWindow(iOffX + pDraw->iX, iOffY + pDraw->iY, pDraw->iWidth, pDraw->iHeight);
+  }
+  // For all other lines, just push the pixels to the display
+  lcd.pushPixels((uint16_t *)pDraw->pPixels, pDraw->iWidth, DRAW_TO_LCD | DRAW_WITH_DMA);
+} /* GIFDraw() */
+
+static void * GIFOpenFile(const char *fname, int32_t *pSize)
+{
+  //log_d("GIFOpenFile( %s )\n", fname );
+  f = SD.open(fname);
+  if (f) {
+    *pSize = f.size();
+    return (void *)&f;
+  }
+  return NULL;
+} /* GIFOpenFile() */
+
+static void GIFCloseFile(void *pHandle)
+{
+  File *f = static_cast<File *>(pHandle);
+  if (f != NULL)
+     f->close();
+}
+
+int32_t GIFReadFile(GIFFILE *pFile, uint8_t *pBuf, int32_t iLen)
+{
+    int32_t iBytesRead;
+    iBytesRead = iLen;
+    File *f = static_cast<File *>(pFile->fHandle);
+    // Note: If you read a file all the way to the last byte, seek() stops working
+    if ((pFile->iSize - pFile->iPos) < iLen)
+       iBytesRead = pFile->iSize - pFile->iPos - 1; // <-- ugly work-around
+    if (iBytesRead <= 0)
+       return 0;
+    iBytesRead = (int32_t)f->read(pBuf, iBytesRead);
+    pFile->iPos = f->position();
+    return iBytesRead;
+} /* GIFReadFile() */
+
+int32_t GIFSeekFile(GIFFILE *pFile, int32_t iPosition)
+{ 
+  int i = micros();
+  File *f = static_cast<File *>(pFile->fHandle);
+  f->seek(iPosition);
+  pFile->iPos = (int32_t)f->position();
+  i = micros() - i;
+//  Serial.printf("Seek time = %d us\n", i);
+  return pFile->iPos;
+} /* GIFSeekFile() */
+
+bool openGIF(const char *fileName)
+{
+    int w, h;
+
+    if (gif.open(fileName, GIFOpenFile, GIFCloseFile, GIFReadFile, GIFSeekFile, GIFDraw)) {
+        w = gif.getCanvasWidth();
+        h = gif.getCanvasHeight();
+        Serial.printf("Canvas size = %d x %d\n", w, h);
+        pFrameBuffer = (uint8_t *)heap_caps_malloc(w*(h+2), MALLOC_CAP_8BIT);
+        gif.setDrawType(GIF_DRAW_COOKED); // we want the library to generate ready-made pixels
+        gif.setFrameBuf(pFrameBuffer);
+        iOffX = (lcd.width() - w)/2;
+        iOffY = (lcd.height() - h)/2;
+        return true;
+    } 
+    else
+    {
+        return false;
+    }
+}
+
+bool initGIF()
+{
+    gif.begin(BIG_ENDIAN_PIXELS);
+    lcd.begin(LCD);
+    lcd.fillScreen(TFT_BLACK);
+    return true;
+}
+
+bool loopGIF()
+{
+    return gif.playFrame(true, NULL);
+}
+
+bool resetGIF()
+{
+    gif.reset();
+    lcd.fillScreen(TFT_BLACK);
+    heap_caps_free(pFrameBuffer);
+    gif.close();
+    return true;
+}
---
+++ src/main.cpp
@@ -0,0 +1,136 @@
+//
+// CYD (Cheap Yellow Display) GIF example
+//
+// #include <bb_captouch.h>
+#include <bb_spi_lcd.h>
+#include <AnimatedGIF.h>
+// #define GIF_NAME earth_128x128
+
+#include <SPI.h>
+#include <SD.h>
+#include "FS.h"
+
+#include <vector>
+#include <algorithm> // For std::shuffle
+#include <random>
+
+#include "audioTask.h"
+#include "gifUtils.h"
+
+char *subDir = "/wade_messages";
+
+char *spash_screen_gif = "/spash_screen.gif";
+char *spash_screen_wav = "/spash_screen.wav";
+
+
+// define SD pins
+#define SD_CS_PIN 5
+#define MOSI_PIN 23
+#define SCK_PIN 18
+#define MISO_PIN 19
+
+bool audioSucceeded = false;
+
+std::vector<String> listGIFFiles(const char* directory) {
+  File root = SD.open(directory);
+  std::vector<String> files;
+  if (!root) {
+    Serial.println("Failed to open directory");
+    return files;
+  }
+  if (!root.isDirectory()) {
+    Serial.println("Not a directory");
+    return files;
+  }
+  File file = root.openNextFile();
+  while (file) {
+    if (String(file.name()).endsWith(".gif")) {
+      files.push_back(String(file.name()));
+    }
+    file = root.openNextFile();
+  }
+  return files;
+}
+
+void setup() {
+  Serial.begin(115200);
+  // randomSeed(analogRead(0));
+
+  SPI.begin(SCK_PIN, MISO_PIN, MOSI_PIN, SD_CS_PIN);
+  if (!SD.begin(SD_CS_PIN, SPI, 80000000)) {
+    Serial.println("SD card init failed!");
+    while (1); // SD initialisation failed so wait here
+  }
+  Serial.println("SD Card init succeeded!");
+
+  auto gifFiles = listGIFFiles(subDir);
+  if (gifFiles.empty()) {
+    Serial.println("No .gif files found");
+    return;
+  }
+  
+  Serial.println("found " + String(gifFiles.size()) + " files");
+
+  // Shuffle the list to randomize which file is played
+  int randomIndex = random(gifFiles.size());
+
+  // Assuming the first file is now a randomly selected one
+  String selectedGIF = gifFiles.at(randomIndex);
+  String selectedWAV = selectedGIF.substring(0, selectedGIF.lastIndexOf('.')) + ".wav";
+  Serial.println("randomly selected message: " + selectedGIF);
+
+  initGIF();
+  // lcd.setTextColor(TFT_GREEN, TFT_BLACK);
+  // lcd.setFont(FONT_12x16);
+  // lcd.setCursor(0, 0);
+  // lcd.println("Test");
+
+  // Setup audio
+  audioInit();
+
+  if(!openGIF(spash_screen_gif))
+  {
+    Serial.println("GIF file open failed");
+  }
+  Serial.println("GIF file open succeeded");
+
+  if(!audioConnecttoFS(SD, spash_screen_wav))
+  {
+    Serial.println("Audio file open failed");
+  }
+  Serial.println("Audio file open succeeded");
+
+  while(loopGIF())
+  {
+    yield();
+  }
+
+  resetGIF();
+  
+  if(!openGIF((String(subDir) + "/" + selectedGIF).c_str()))
+  {
+    Serial.println("GIF file open failed");
+  }
+  Serial.println("GIF file open succeeded");
+
+  if(!audioConnecttoFS(SD, (String(subDir) + "/" + selectedWAV).c_str()))
+  {
+    Serial.println("Audio file open failed");
+  }
+  Serial.println("Audio file open succeeded");
+}
+
+// TODO: look into new example in AnimatedGIF
+
+void loop() {
+  if (loopGIF())
+  {
+    // do stuff while gif os playing
+  }
+  else
+  {
+    Serial.println("Done playing, entering deep sleep");
+    esp_deep_sleep_start();
+  }
+  yield();
+}
